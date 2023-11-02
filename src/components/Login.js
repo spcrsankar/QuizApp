6ed09b7ceb5af3cc01d1aba76bb2d2577abb2ecb
spcrsankar <sankaralingam2910@gmail.com>
2023-11-02 14:16:23 +0530

    solve image not loading problem

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import "../assets/email.png"
 
 export default function Login() {
  
